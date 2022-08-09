b6dbdb32b3c7290aa568e98a3f67c0edce7a858b
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-08-09 12:35:45 +0530

    Navbar created

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import "./Navbar.scss";
+
+export const Navbar = () => {
+    const [active, setActive] = useState("");
+    const openLinks = () => {
+        active === "" ? setActive("active") : setActive("");
+    }
+    return (
+        <div className="navbar">
+            <div className={"links " + active}>
+                <a href="./">Home</a>
+                <a href="./">About</a>
+            </div>
+            <div className={"hamburger " + active} onClick={() => openLinks()}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
+        </div>
+    );
+}
