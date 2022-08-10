060bc27d42f35238b7e9ea30efff2b76928342f7
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-08-10 22:32:59 +0530

    Created main/splash page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
 import './App.css';
 import { Navbar } from './components/navbar/Navbar';
 import { Table } from './components/table/Table';
 import { Testimonials } from './components/testimonials/Testimonials';
+import { Main } from './components/main/Main';
+
 function App() {
   return (
     <div className="App">      
       <Navbar />
+      <Main/>
       <div style={{marginTop: "100px" }}>
         <div className="table-container-box">
           <h1>School Toppers</h1>
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Main.scss";
+
+export const Main = () => {
+    return (
+        <div className="splash">
+            <h1>DPS RK Puram</h1>
+            <p>The best school, with the best results</p>
+        </div>
+    );
+}
