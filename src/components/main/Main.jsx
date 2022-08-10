b9e6bf9ec3d6cd5c05941f01def1d4550fb5c772
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-08-10 23:29:33 +0530

    Minor fix

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -5,7 +5,7 @@ export const Main = () => {
     return (
         <div className="splash">
             <h1>DPS RK Puram</h1>
-            <p>The best school, with the best results</p>
+            <p>"Open doors and open minds..."</p>
         </div>
     );
 }
