8f4105ea0db42a3484426a1f6783767dd2bd41d0
Tejas Baid <tejasbaid3@gmail.com>
2022-08-10 12:33:54 +0530

    Feat: Table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import { Navbar } from './components/navbar/Navbar';
+import { Table } from './components/table/Table';
 import { Testimonials } from './components/testimonials/Testimonials';
 import { Toppers } from './components/toppers/Toppers';
 function App() {
@@ -13,7 +14,7 @@ function App() {
           
         </div>
       <div style={{marginTop: "100px" }}>
-        
+        <div className="table-container-box"><Table /></div>
         <Testimonials />
       </div>
     </div>
